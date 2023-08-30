5c64e89892d35557b31699a9ee124c3349d758ad
shigenogoro <kyle990987@gmail.com>
2023-08-30 16:17:02 +0800

    next_permutation practice

--- epi_judge_cpp/next_permutation.cc
+++ epi_judge_cpp/next_permutation.cc
@@ -2,9 +2,39 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::swap;
 vector<int> NextPermutation(vector<int> perm) {
   // TODO - you fill in here.
-  return {};
+  // Step1: Find the start entry that has the longest decreasing suffix from the right
+  int pivotEntry = 0;
+  for(int i = perm.size() - 2; i >= -1; i--) {
+      if(perm[i] < perm[i + 1]) {
+          pivotEntry = i;
+          break;
+      } else if(i < 0) {
+          pivotEntry = -1;
+      }
+  }
+  // the perm is the largest permutation -> next permutation is null
+  if(pivotEntry == -1) {
+      return {};
+  }
+
+  // Step 2: Find the smallest entry such that perm[smallestEntry] > perm[pivotEntry]
+  int smallestEntry = pivotEntry + 1;
+  for(int i = pivotEntry + 2; i < perm.size(); i++) {
+      if(perm[i] > perm[pivotEntry] && perm[smallestEntry] > perm[i]) {
+          smallestEntry = i;
+      }
+  }
+
+  // Step 3: Swap perm[smallestEntry] and perm[pivotEntry]
+  swap(perm[smallestEntry], perm[pivotEntry]);
+
+  // Step 4: Reverse the sequence after position pivotEntry -> sort the entries in the suffix from smallest to largest
+  sort(perm.begin() + pivotEntry + 1, perm.end());
+
+  return perm;
 }
 
 int main(int argc, char* argv[]) {
